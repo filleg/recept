a146ca404afbf63277e2bc0c599b9ca3e7211055
filleg <50096970+filleg@users.noreply.github.com>
2021-02-04 16:46:18 +0100

    Sidebar

--- src/components/backdrop/BackDrop.tsx
+++ src/components/backdrop/BackDrop.tsx
@@ -1,5 +1,5 @@
 import './BackDrop.css'
 
 export const BackDrop = (props: { drawerHandler: Function}) => {
-    return (<div onClick={() => props.drawerHandler(false)} className='backdrop'/>)
+    return (<div onClick={() => props.drawerHandler(false)} className='backDrop'/>)
 }
