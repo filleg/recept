31e065fb7272ff9bab2c77666b143433b58a1760
filleg <50096970+filleg@users.noreply.github.com>
2021-03-04 15:06:12 +0100

    config backend

--- server/src/controllers/User.controller.js
+++ server/src/controllers/User.controller.js
@@ -1,3 +1,4 @@
+import { request, response } from 'express'
 import UserModel from '../models/User.model.js'
 
 const createUser = async (request, response) => {
@@ -28,7 +29,65 @@ const getAllUsers = async (request, response) => {
 	}
 }
 
+const deleteUser = async (request, response) => {
+	try {
+		const userId = request.params.userId
+		const databaseResponse = await UserModel.findByIdAndDelete(request.params.userId)
+		response.status(200).send({message: 'Användare borttagen', date: databaseResponse})
+	} catch (error) {
+		response.status(500).send({
+			message: `Gick inte att ta bort användare med ID ${userId}`,
+			error: error.message
+		})
+	}
+}
+
+const updateUser = async (request, response) => {
+	const userId = request.params.userId
+	const data = {
+		username: request.body.username,
+		password: request.body.password
+	}
+	try {
+		const databaseResponse = await UserModel.findByIdAndUpdate(userId, data, { new: true })
+		response.status(200).send(databaseResponse)
+	} catch(error) {
+		response.status(500).send({
+			message: `Gick inte att uppdatera info om användare med ID ${userId}`,
+			error: error.message
+		})
+	}
+}
+
+const queryUsername = async (request, response) => {
+	try {
+		const databaseResponse = await UserModel.find({ username: request.query.username })
+		response.status(200).send(databaseResponse)
+	} catch (error) {
+		response.status(500).send({
+			message: `Gick inte att hitta användaren: ${request.query.username}`,
+			error: error.message
+		})
+	}
+}
+
+const getUserByID = async (request, response) => {
+	try {
+		const databaseResponse = await UserModel.findOne({ _id: request.params.userId })
+		response.status(200).send(databaseResponse)
+	} catch (error) {
+		response.status(500).send({
+			message: `Gick inte att hitta användaren med ID: ${request.params.userId}`,
+			error: error.message
+		})
+	}
+}
+
 export default {
 	createUser,
-	getAllUsers
+	getAllUsers,
+	deleteUser,
+	updateUser,
+	queryUsername,
+	getUserByID
 }
--- server/src/routes/User.route.js
+++ server/src/routes/User.route.js
@@ -3,6 +3,10 @@ import UserController from '../controllers/User.controller.js'
 const routes = application => {
 	application.post('/user', UserController.createUser)
 	application.get('/user', UserController.getAllUsers)
+	application.delete('/user/:userId', UserController.deleteUser)
+	application.put('/user/:userId', UserController.updateUser)
+	application.get('/search', UserController.queryUsername)
+	application.get('/user/:userId', UserController.getUserByID)
 }
 
 export default { routes }
